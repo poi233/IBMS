4c3c2dd2d4b16aa562be8522308ab04bbea701a0
poi233 <593657039@qq.com>
2016-12-27 18:02:19 +0800

    add project view

--- application/controllers/SystemManage/Project.php
+++ application/controllers/SystemManage/Project.php
@@ -7,5 +7,17 @@
  */
 class Project extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->load->model('Project_model');
+    }
 
+    public function index()
+    {
+        $this->load->view('projectSystem/projects_install');
+    }
 }
